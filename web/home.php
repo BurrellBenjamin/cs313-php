daff9a54aed2619a72a8fe328e5ea463e8bad97d
Ben Burrell <benjaminjburrell@gmail.com>
2018-09-29 21:00:22 -0700

    changed the src attribute on stylesheet import to href, hope this fixes things...

--- web/home.php
+++ web/home.php
@@ -2,7 +2,7 @@
 <html>
     <head>
         <meta charset="utf-8">
-        <link rel="stylesheet" type="text/css" src="main.css">
+        <link rel="stylesheet" type="text/css" href="main.css">
         <title>Ben Burrell's Homepage</title>
     </head>
     <body id="mainbody">
