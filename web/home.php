7bcf60bd72e8aa80500f9b226706fe86006a9b26
Ben Burrell <benjaminjburrell@gmail.com>
2018-09-29 20:07:59 -0700

    testing for ponder 02

---
+++ web/home.php
@@ -0,0 +1,41 @@
+<!doctype html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <link rel="stylesheet" src="main.css">
+        <title>Ben Burrell's Homepage</title>
+    </head>
+    <body>
+        <header>
+            <?php include 'menu.php'; ?>
+        </header>
+        <main>
+            <p>My name is Ben Burrell. I live with my wife and puppy, and I
+                enjoy playing video games. My favorite game is Pokemon, and
+                I'm in the midst of starting up my own competitive Draft League.
+                (details forthcoming). Two of my personal favorite Pokemon to play
+                with are Claydol and Dusclops, because they both have high defenses.
+            </p>
+            <img src="claydol.png" alt="#344-Claydol" id="claydol">
+            <img src="dusclops.png" alt="#356-Dusclops" id="dusclops">
+            <br>
+            <p>Competitive Pokemon might seem like a silly idea to the uninitiated,
+                but those people haven't seen the math behind the game's primary
+                mechanics. This handy equation below calculates the damage that a
+                given (damaging) move does, with a little random number variation
+                for extra fun and frustration.
+            </p>
+            <img src="damageCalc.png" alt="Damage Calculation formula for Pokemon" href="https://bulbapedia.bulbagarden.net/wiki/Damage">
+            <br>
+            <p>Math aside, the sheer volume of critical thinking that goes into
+                competitive Pokemon is staggering enough to convince scoffers of
+                the validity of the term. If you still want proof, I'd invite you
+                to watch this video where a Pokemon Youtuber legitimately takes
+                30 minutes to plan for his match.
+            </p>
+            <iframe src="https://www.youtube.com/embed/mWvnCM3x_-k" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
+            <br>
+            
+        </main>
+    </body>
+</html>
---
+++ web/menu.php
@@ -0,0 +1,4 @@
+<?php
+    echo '<a href="home.php">Home</a> -
+    <a href="assignments.html">Assignments</a>';
+?>
