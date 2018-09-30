b12626899e33184a9ac06ffa34e434a0045dd75c
Ben Burrell <benjaminjburrell@gmail.com>
2018-09-29 20:33:45 -0700

    updated stylesheet reference

--- web/home.php
+++ web/home.php
@@ -2,7 +2,7 @@
 <html>
     <head>
         <meta charset="utf-8">
-        <link rel="stylesheet" src="main.css">
+        <link rel="stylesheet" type="text/css" src="main.css">
         <title>Ben Burrell's Homepage</title>
     </head>
     <body>
