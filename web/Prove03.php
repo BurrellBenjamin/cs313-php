0c4b790ef08e50aa410dc76fa4b2a2f4f0483cbd
Ben Burrell <benjaminjburrell@gmail.com>
2018-10-08 21:03:01 -0700

    Prove 03-Stage 2

--- web/Prove03.php
+++ web/Prove03.php
@@ -11,51 +11,54 @@
             <h1>Welcome to the Online Grocery Store!</h1>
         </header>
         <main>
-            <div  id="productTable">
-                <?php 
-                #require 'cart.php';
-                require 'listings.php';
-                $salesArray1 = array();
-                $salesArray2 = array();
-                $salesArray3 = array();
-                $salesIndex = 0;
-                for($x=0; $x<$arrlength; $x++)
-                {
-                    if($x % 3 == 0)
+            <form action="Prove03.php" method="post">
+                <div  id="productTable">
+                    <?php 
+                    require 'cart.php';
+                    require 'listings.php';
+                    $salesArray1 = array();
+                    $salesArray2 = array();
+                    $salesArray3 = array();
+                    $salesIndex = 0;
+                    for($x=0; $x<$arrlength; $x++)
                     {
-                        $salesArray1[$salesIndex] = $listings[$x];
+                        if($x % 3 == 0)
+                        {
+                            $salesArray1[$salesIndex] = $listings[$x];
+                        }
+                        else if ($x % 3 == 1)
+                        {
+                            $salesArray2[$salesIndex] = $listings[$x]; 
+                        }
+                        else #if $x %3 == 2
+                        {
+                            $salesArray3[$salesIndex] = $listings[$x];
+                            $salesIndex++;
+                        }
                     }
-                    else if ($x % 3 == 1)
+                    echo '<div id="left" class="column">';
+                    for($x=0; $x<count($salesArray1); $x++)
                     {
-                        $salesArray2[$salesIndex] = $listings[$x]; 
+                        echo $salesArray1[$x];
+                        echo "<br>";
+                        echo "<input type='submit' name='1-$x' value='1-$x'> $salesArray1[$x] </input>";
                     }
-                    else #if $x %3 == 2
+                    echo '</div><div id="middle" class="column">';
+                    for($x=0; $x<count($salesArray2); $x++)
                     {
-                        $salesArray3[$salesIndex] = $listings[$x];
-                        $salesIndex++;
+                        echo $salesArray2[$x];
+                        echo "<br>";
                     }
-                }
-                echo '<div id="left" class="column">';
-                for($x=0; $x<count($salesArray1); $x++)
-                {
-                    echo $salesArray1[$x];
-                    echo "<br>";
-                }
-                echo '</div><div id="middle" class="column">';
-                for($x=0; $x<count($salesArray2); $x++)
-                {
-                    echo $salesArray2[$x];
-                    echo "<br>";
-                }
-                echo '</div><div id="right" class="column">';
-                for($x=0; $x<count($salesArray3); $x++)
-                {
-                    echo $salesArray3[$x];
-                    echo "<br>";
-                }
-                echo '</div>';
-                ?> 
-            </div>
+                    echo '</div><div id="right" class="column">';
+                    for($x=0; $x<count($salesArray3); $x++)
+                    {
+                        echo $salesArray3[$x];
+                        echo "<br>";
+                    }
+                    echo '</div>';
+                    ?> 
+                </div>
+            </form>
         </main>
     </body>
 </html>
--- web/cart.php
+++ web/cart.php
@@ -1,4 +1,12 @@
 <?php
-echo "Import Successful.";
+session_start();
 
+$_SESSION[cart] = array();
+$_SESSION[cartSize] = 0;
+
+function addItems($itemIndex)
+{
+    $_SESSION[cart][$_SESSION[cartSize]] = $itemIndex;
+    $_SESSION[cartSize]++;
+}
 ?>
