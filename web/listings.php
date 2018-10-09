182fa2327eafa19eb7283a54e609f5b397aece66
Ben Burrell <benjaminjburrell@gmail.com>
2018-10-08 19:51:37 -0700

    Prove 03

--- web/listings.php
+++ web/listings.php
@@ -1,5 +1,5 @@
 <?php
 $listings = array("Bananas", "Pinapples (gross)", "Plums", "Pluots", "White Peaches", "Rasperries", "Leeks", "Yellow Peaches", "Italian Squash", "Yellow Squash", "Acorn Squash", "Butternut Squash", "Passionfruit", "Dragonfruit", "Starfruit", "Cherimoya", "Pomegranates", "Carrots", "Brussel Sprouts");
 sort($listings);
-$arrlength = count(!listings);
+$arrlength = count($listings);
 ?>
