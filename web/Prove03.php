738bd7e2c04f43c2001d09463843c964160b7002
Ben Burrell <benjaminjburrell@gmail.com>
2018-10-08 19:59:38 -0700

    Prove 03

--- web/Prove03.php
+++ web/Prove03.php
@@ -10,50 +10,52 @@
             
             <h1>Welcome to the Online Grocery Store!</h1>
         </header>
-        <main id="container">
-            <?php 
-            #require 'cart.php';
-            require 'listings.php';
-            $salesArray1 = array();
-            $salesArray2 = array();
-            $salesArray3 = array();
-            $salesIndex = 0;
-            for($x=0; $x<$arrlength; $x++)
-            {
-                if($x % 3 == 0)
+        <main>
+            <div  id="container">
+                <?php 
+                #require 'cart.php';
+                require 'listings.php';
+                $salesArray1 = array();
+                $salesArray2 = array();
+                $salesArray3 = array();
+                $salesIndex = 0;
+                for($x=0; $x<$arrlength; $x++)
                 {
-                    $salesArray1[$salesIndex] = $listings[$x];
+                    if($x % 3 == 0)
+                    {
+                        $salesArray1[$salesIndex] = $listings[$x];
+                    }
+                    else if ($x % 3 == 1)
+                    {
+                        $salesArray2[$salesIndex] = $listings[$x]; 
+                    }
+                    else #if $x %3 == 2
+                    {
+                        $salesArray3[$salesIndex] = $listings[$x];
+                        $salesIndex++;
+                    }
                 }
-                else if ($x % 3 == 1)
+                echo '<div id="left">';
+                for($x=0; $x<count($salesArray1); $x++)
                 {
-                    $salesArray2[$salesIndex] = $listings[$x]; 
+                    echo $salesArray1[$x];
+                    echo "<br>";
                 }
-                else #if $x %3 == 2
+                echo '</div><div id="middle">';
+                for($x=0; $x<count($salesArray2); $x++)
                 {
-                    $salesArray3[$salesIndex] = $listings[$x];
-                    $salesIndex++;
+                    echo $salesArray2[$x];
+                    echo "<br>";
                 }
-            }
-            echo '<div id="left">';
-            for($x=0; $x<count($salesArray1); $x++)
-            {
-                echo $salesArray1[$x];
-                echo "<br>";
-            }
-            echo '</div><div id="middle">';
-            for($x=0; $x<count($salesArray2); $x++)
-            {
-                echo $salesArray2[$x];
-                echo "<br>";
-            }
-            echo '</div><div id="right">';
+                echo '</div><div id="right">';
                 for($x=0; $x<count($salesArray3); $x++)
-            {
-                echo $salesArray3[$x];
-                echo "<br>";
-            }
-            echo '</div>';
-               ?> 
+                {
+                    echo $salesArray3[$x];
+                    echo "<br>";
+                }
+                echo '</div>';
+                ?> 
+            </div>
         </main>
     </body>
 </html>
