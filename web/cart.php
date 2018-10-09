d9cb72857199c14089273731ebf297147b5c2620
Ben Burrell <benjaminjburrell@gmail.com>
2018-10-08 23:05:49 -0700

    Prove 03-Stage 2

--- web/cart.php
+++ web/cart.php
@@ -1,6 +1,9 @@
 <?php
-if($_SERVER['REQUEST_METHOD'] == "POST" and isset($_POST))
+if($_SERVER['REQUEST_METHOD'] == "POST" and isset($_POST['remove']))
 {
+    removeItems($_POST['remove']);
+}
+else if($_SERVER['REQUEST_METHOD'] == "POST" and isset($_POST)){
     foreach($_POST as $x => $x_val)
     {
         addItems($_POST[$x]);
@@ -11,5 +14,15 @@ function addItems($itemIndex)
 {
     $_SESSION["cart"][$_SESSION["cartSize"]] = $itemIndex;
     $_SESSION["cartSize"]++;
+}
+
+function removeItems($cartIndex)
+{
+    for($x=$cartIndex; $x < $_SESSION["cartSize"] -1; $x++)
+    {
+        $_SESSION["cart"][$x] = $_SESSION["cart"][$x + 1];
+    }
+    $_SESSION["cart"][$_SESSION["cartSize"]] = null;
+    $_SESSION["cartSize"]--;
 }
 ?>
--- web/checkout.php
+++ web/checkout.php
@@ -12,17 +12,20 @@
             <hr>
         </header>
         <main>
-            <ul>
-                <?php
-                    require 'cart.php';
-                for($x = 0; $x < $_SESSION["cartSize"]; $x++)
-                {
-                    echo '<li>';
-                    echo $_SESSION["cart"][$x];
-                    echo '</li>';
-                }
+            <form method="post" action="checkout.php">
+                <ul>
+                    <?php
+                        require 'cart.php';
+                        for($x = 0; $x < $_SESSION["cartSize"]; $x++)
+                        {
+                            echo "<li>";
+                            echo $_SESSION["cart"][$x];
+                            echo "<input type='submit' name='remove' value=$x>";
+                            echo "</li>";
+                        }
                     ?>
-            </ul>
+                </ul>
+            </form>
             <a href="Prove03.php">Add More to Cart</a> - - <a href="delivery.php">Finish Checking Out</a>
         </main>
     </body>
