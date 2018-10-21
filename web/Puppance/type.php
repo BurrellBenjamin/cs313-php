3797a9f619b2580bd335f34be6e7d9f3fd5fa3bd
Ben Burrell <benjaminjburrell@gmail.com>
2018-10-21 01:25:25 -0700

    Ponder 05 update, Addressed Puppance error in text display.

--- web/Puppance/type.php
+++ web/Puppance/type.php
@@ -36,7 +36,7 @@
                     
                     echo "<hr><br><br><br><h3>Moves of this type</h3><hr>";
                     foreach($db->query("select * from move_table where typing = '" . htmlspecialchars($_GET["id"]) . "' order by name") as $row){
-                        echo "<a href='move.php?id=" .  $row['name'] . "'>" . $row['name'] . "</a>\t| <a href='type.php?id=" . $row['typing']  . "'>" . $row['typing'] . "</a>" . "\t| " . $row['style'] . "\t| " . $row['power'] . "\t| " . $row['accuracy'] . "\t| " . $row['detail'];
+                        echo "<a href='move.php?id=" .  $row['name'] . "'>" . $row['name'] . "</a>\t| <a href='type.php?id=" . $row['typing']  . "'>" . $row['typing'] . "</a>" . "\t| " . $row['style'] . "\t| " . $row['power'] . "\t| " . $row['accuracy'] . "\t| " . $row['detail'] . "<br>";
                     }
                 }
                 catch(PDOException $ex){
