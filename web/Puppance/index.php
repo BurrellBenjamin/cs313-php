2409e0309775a99edd6196b6048bd2a4230aaae3
Ben Burrell <benjaminjburrell@gmail.com>
2018-10-21 04:24:54 -0700

    Prove 05 Update. Fancified the pages.

--- web/Puppance/index.php
+++ web/Puppance/index.php
@@ -19,42 +19,6 @@
                 <input type="text" name="name"><br>
                 <input type="submit">
             </form>
-            <?php
-                try
-                {
-                    $dburl = getenv('DATABASE_URL');
-                    
-                    $db0pts = parse_url($db_url);
-
-
-                    $dbHost = $dbOpts["host"];
-                    $dbPort = $dbOpts["port"];
-                    $dbUser = $dbOpts["user"];
-                    $dbPassword = $dbOpts["pass"];
-                    $dbName = ltrim($dbOpts["path"],'/');
-
-                    $db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword);
-
-                    $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-                    
-                    if(isset($_POST["name"]))
-                    {
-                        foreach($db->query("select * from pokemon_table where name like '%" . htmlspecialchars($_POST['name']) . "%'") as $row){
-                            echo "<a href='pokemon.php?id=" . $row["name"] . "'> " . $row["name"] . "</a>\t| <a href='type.php?id=" . $row['typing1']  . ">" . $row['typing1'] . "</a>";
-                            if($row['typing2'] != null)
-                                echo " / <a href='type.php?id=" .$row['typing2'] . ">" . $row['typing2'] . "</a>\t| ";
-                            else
-                                echo "\t\t| ";
-                            echo "<a href='ability.php?id=" . $row['ability1'] . "'>" . $row['ability1'] . "</a>" . "\t| <a href='" . $row['ability2'] . "'>" . $row['ability2'] . "</a>\t| <a href='" . $row['hiddenability'] . "'>" . $row['hiddenability'] . "</a>\t| <a href='" . $row['tier'] . "'>" . $row['tier'] . "</a><hr>";
-                        }
-                    }
-                }
-                catch(PDOException $ex)
-                {
-                    echo'Error!: ' . $ex->getMessage();
-                    die();
-                }
-            ?>
         </main>
 	</body>
 </html>
