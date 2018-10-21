a00b2119d42c09c51472a60870955eab0e8aea8a
Ben Burrell <benjaminjburrell@gmail.com>
2018-10-21 01:22:15 -0700

    Ponder 05 update, Addressed Puppance error in text display.

--- web/Puppance/type.php
+++ web/Puppance/type.php
@@ -34,7 +34,7 @@
                         echo "\t\t| <a href='ability.php?id=" . $row['ability1'] . "'>" . $row['ability1'] . "</a>" . "\t| <a href='ability.php?id=" . $row['ability2'] . "'>" . $row['ability2'] . "</a>\t| <a href='ability.php?id=" . $row['hiddenability'] . "'>" . $row['hiddenability'] . "</a>\t| <a href='tier.php?id=" . $row['tier'] . "'>" . $row['tier'] . "</a><br>";
                     }
                     
-                    echo "<hr><br><br><br><h3>Moves of this type</h3<hr>";
+                    echo "<hr><br><br><br><h3>Moves of this type</h3><hr>";
                     foreach($db->query("select * from move_table where typing = '" . htmlspecialchars($_GET["id"]) . "' order by name") as $row){
                         echo "<a href='move.php?id=" .  $row['name'] . "'>" . $row['name'] . "</a>\t| <a href='type.php?id=" . $row['typing']  . ">" . $row['typing'] . "</a>" . "\t| " . $row['style'] . "\t| " . $row['power'] . "\t| " . $row['accuracy'] . "\t| " . $row['detail'];
                     }
