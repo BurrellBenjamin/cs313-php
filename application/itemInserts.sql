1995e186ad25191a4b97b0bfbfe6b152864ac9a8
Ben Burrell <benjaminjburrell@gmail.com>
2018-10-24 09:43:41 -0700

    updated item listings and item.php

--- application/itemInserts.sql
+++ application/itemInserts.sql
@@ -1,7 +1,44 @@
+
 insert into item_table 
 	(index, name, detail)
-	value (nextval('s_items'), '', '');
+	values (nextval('s_items'), '', '');
 
 insert into item_table 
 	(index, name, detail)
 	values (nextval('s_items'), 'Alakazite', 'To be held by Alakazam. Allows Alakazam to Mega Evolve.');
+
+insert into item_table 
+	(index, name, detail)
+	values (nextval('s_items'), 'Life Orb', 'Holder''s attacks deal 1.3x damage, and it loses 1/10 its max HP after the attack.');
+
+insert into item_table 
+	(index, name, detail)
+	values (nextval('s_items'), 'Choice Scarf', 'The holder enters the field with 1.5x Speed, but can only use the first move they choose upon being sent in.');
+
+insert into item_table 
+	(index, name, detail)
+	values (nextval('s_items'), 'Choice Specs', 'The holder enters the field with 1.5x Special Attack, but can only use the first move they choose upon being sent in.');
+
+insert into item_table 
+	(index, name, detail)
+	values (nextval('s_items'), 'Choice Band', 'The holder enters the field with 1.5x Attack, but can only use the first move they choose upon being sent in.');
+
+insert into item_table 
+	(index, name, detail)
+	values (nextval('s_items'), 'Eviolite', 'If the holder is not a fully-evolved Pokemon, it receives 1.5x to both Defense and Special Defense.');
+
+insert into item_table 
+	(index, name, detail)
+	values (nextval('s_items'), 'Expert Belt', 'The holder''s super-effectice attacks do 1.2x damage.');
+
+insert into item_table 
+	(index, name, detail)
+	values (nextval('s_items'), 'Leftovers', 'The holder recovers 1/16 of its max HP at the end of every turn.');
+
+insert into item_table 
+	(index, name, detail)
+	values (nextval('s_items'), 'Lum Berry', 'When the holder becomes burned, paralyzed, poisoned, asleep, or confused, this item is consumed, and the holder cures itself.');
+
+insert into item_table 
+	(index, name, detail)
+	values (nextval('s_items'), 'Sitrus Berry', 'When the holder''s HP drops below 1/2 of their maximum HP, this item is consumed, and the holder recovers 1/4 their maximum HP');
--- application/setInserts.sql
+++ application/setInserts.sql
@@ -1,3 +1,4 @@
+
 insert into set_table 
 	(index, name, pokemon, item, ability, nature, hp, attack, defense, spattack, spdefense, speed, move1, move2, move3, move4, detail)
 	values (nextval('s_sets'), '', '', '', '', '', , , , , , , '', '', '', '', '');
@@ -5,3 +6,7 @@ insert into set_table
 insert into set_table 
 	(index, name, pokemon, item, ability, nature, hp, attack, defense, spattack, spdefense, speed, move1, move2, move3, move4, detail)
 	values (nextval('s_sets'), 'Special Sweeper', 'Alakazam', 'Alakazite', 'Magic Guard', 'Timid', 0, 0, 4, 252, 0, 252, 'Psychic', 'Focus Blast', 'Recover', 'Calm Mind', '');
+
+insert into set_table 
+	(index, name, pokemon, item, ability, nature, hp, attack, defense, spattack, spdefense, speed, move1, move2, move3, move4, detail)
+	values (nextval('s_sets'), 'Contrary Sweeper', 'Serperior', 'Life Orb', 'Contrary', 'Timid', 0, 0, 0, 252, 4, 252, 'Leaf Storm', 'Giga Drain', 'Dragon Pulse', 'Hidder Power Fire', 'This set relies on Serperior landing a single Leaf Storm, which in conjunction with Contrary, boosts its Special Attack by 2 stages every time it''s used.');
