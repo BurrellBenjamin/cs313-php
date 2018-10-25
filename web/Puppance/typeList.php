6357638847c1e91c8d2c0945e638082d59eb2a11
Ben Burrell <benjaminjburrell@gmail.com>
2018-10-24 18:10:08 -0700

    Fixed an error on Pokemon.php that incorrectly displayed a Pokemon's secondary typing. Also reordered the typing enum so typings display in alphabetical order on typelist.php

--- web/Puppance/typeList.php
+++ web/Puppance/typeList.php
@@ -26,7 +26,7 @@
                     
                     echo "<h1>Types</h1><hr>";
                     
-                    foreach($db->query("select * from unnest(enum_range('fairy'::typing, 'normal'::typing))") as $row){
+                    foreach($db->query("select * from unnest(enum_range('bug'::typing, 'water'::typing))") as $row){
                         echo "<a href='type.php?id=" .  $row['unnest'] . "'>" . $row['unnest'] . "</a><hr>";
                     }
                     
